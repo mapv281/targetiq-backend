de622bf8b5733a0c5f7acb3d4beb55c2bf20ab26
Mauricio <mapv1@hotmail.com>
2025-07-30 15:58:07 -0500

    html5 response fix

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ async def detect_bullet_holes_with_openai(image_path: str) -> ScoreResult:
             "You are given an image of a paper shooting target from uploaded image with information about the shooter's handedness, dominant eye, distance from target, firearm make, firearm model, firearm caliber, target type, and whether the shooting range is indoor or outdoor. "
             "The shooter's information is as follows: Shooter's name is Mauricio Patino, Handedness is left, Dominant eye is left, Training goals is self-defense,  Distance from target is 7 yards, Firearm make  Glock, Firearm model is 34 Gen4, Firearm ammunition is 9mm Luger, Target type is B-3 Orange, Location is Indoor Range. "
 "Provide total shots, shot group pattern, shot vertical pattern, shot distribution overview, coaching analysis, corrective drills, analysis, recommendations, and suggestions for improvement."
-             "Respond in JSON format like: {\"html_response\": text}  with value in HTML5 format embedded inside a <div> tag"
+             "Respond in compact JSON format like: {\"html_response\": text}  with value in HTML5 format embedded inside a <div> tag"
         )
 
         response = openai.ChatCompletion.create(
